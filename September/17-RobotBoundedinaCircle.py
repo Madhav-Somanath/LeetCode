9cfb4c6abbfb823d7b32308de83396a222a10a1b
Madhav-Somanath <madhav.somanath@gmail.com>
2020-09-17 12:48:02 +0530

    Geometry

---
+++ September/17-RobotBoundedinaCircle.py
@@ -0,0 +1,25 @@
+""" On an infinite plane, a robot initially stands at (0, 0) and faces north.  The robot can receive one of three instructions:
+
+    "G": go straight 1 unit;
+    "L": turn 90 degrees to the left;
+    "R": turn 90 degress to the right.
+
+The robot performs the instructions given in order, and repeats them forever.
+
+Return true if and only if there exists a circle in the plane such that the robot never leaves the circle. """
+
+class Solution:
+    def isRobotBounded(self, instructions: str) -> bool:
+        x, y, dx, dy = 0, 0, 0, 1
+        
+        for i in instructions:
+            if i == 'R':
+                dx, dy = dy, -dx
+            if i == 'L':
+                dx, dy = -dy, dx
+            if i == 'G':
+                x, y = x + dx, y + dy
+                
+        return (x, y) == (0, 0) or (dx, dy) != (0,1)
+    
+# Robot should be at origin or in a direction other than north so that eventually it comes back to origin. O(N)
