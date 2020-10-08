0b65ffefdebd5bfede20a3020ddb4ded31ca687e
Madhav-Somanath <madhav.somanath@gmail.com>
2020-10-08 12:40:42 +0530

    Using Bisect

---
+++ October/8-BinarySearch.py
@@ -0,0 +1,21 @@
+""" Given a sorted (in ascending order) integer array nums of n elements and a target value,
+write a function to search target in nums. If target exists, then return its index, otherwise return -1. """
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        index = bisect.bisect_left(nums, target)
+        return index if index < len(nums) and nums[index] == target else -1
+    
+    '''
+    l, r = 0, len(nums) - 1
+    while l <= r:
+        mid = (l + r) // 2
+        if nums[mid] < target:
+            l = mid + 1
+        elif nums[mid] > target:
+            r = mid - 1
+        else:
+            return mid
+    return -1
+    '''
+        
