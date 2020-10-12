771df61da48eeb9979a910d260401b8262975e5b
Madhav-Somanath <madhav.somanath@gmail.com>
2020-10-12 12:52:56 +0530

    Check difference O(N)

---
+++ October/12-BuddyStrings.py
@@ -0,0 +1,10 @@
+""" Given two strings A and B of lowercase letters, return true if you can swap two letters in A so the result is equal to B, otherwise, return false.
+
+Swapping letters is defined as taking two indices i and j (0-indexed) such that i != j and swapping the characters at A[i] and A[j]. For example, swapping at indices 0 and 2 in "abcd" results in "cbad". """
+
+class Solution:
+    def buddyStrings(self, A: str, B: str) -> bool:
+        if len(A) != len(B): return False
+        if A == B and len(set(A)) < len(A): return True
+        dif = [(a, b) for a, b in zip(A, B) if a != b]
+        return len(dif) == 2 and dif[0] == dif[1][::-1]
