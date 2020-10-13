118ae2a96462953c1ace024a498a3d8aaf2c9a2f
Madhav-Somanath <madhav.somanath@gmail.com>
2020-10-13 17:48:45 +0530

    NOT O(1) SPACE (using recursion)

---
+++ October/13-SortList.py
@@ -0,0 +1,33 @@
+""" Given the head of a linked list, return the list after sorting it in ascending order.
+
+Follow up: Can you sort the linked list in O(n logn) time and O(1) memory (i.e. constant space)? """
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def merge(self, h1, h2):
+        dummy = tail = ListNode(None)
+        while h1 and h2:
+            if h1.val < h2.val:
+                tail.next, tail, h1 = h1, h1, h1.next
+            else:
+                tail.next, tail, h2 = h2, h2, h2.next
+    
+        tail.next = h1 or h2
+        return dummy.next
+    
+    def sortList(self, head: ListNode) -> ListNode:
+        if not head or not head.next:
+            return head
+    
+        pre, slow, fast = None, head, head
+        while fast and fast.next:
+            pre, slow, fast = slow, slow.next, fast.next.next
+        pre.next = None
+
+        return self.merge(*map(self.sortList, (head, slow)))
+        
