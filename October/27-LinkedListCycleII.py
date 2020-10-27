9559f222040dd1fa36d654f4d0a3fc102b9302ef
Madhav-Somanath <madhav.somanath@gmail.com>
2020-10-27 14:28:23 +0530

    Fast and slow pointers

---
+++ October/27-LinkedListCycleII.py
@@ -0,0 +1,31 @@
+""" Given a linked list, return the node where the cycle begins. If there is no cycle, return null.
+
+There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer. Internally, pos is used to denote the index of the node that tail's next pointer is connected to. Note that pos is not passed as a parameter.
+
+Notice that you should not modify the linked list.
+
+Follow up:
+
+Can you solve it using O(1) (i.e. constant) memory? """
+
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    def detectCycle(self, head: ListNode) -> ListNode:
+        slow = fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                break
+        else:
+            return None
+        while head != slow:
+            slow = slow.next
+            head = head.next
+        return head
